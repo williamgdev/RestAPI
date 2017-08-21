14bf61748aff24bb44515cf11913a859ea8834a4
William Gutierrez <williamg.dev@gmail.com>
2017-08-21 11:17:20 -0500

    Change the value to 15

--- src/main/java/com/github/williamgdev/example/submodules/restapi/RestAPI.java
+++ src/main/java/com/github/williamgdev/example/submodules/restapi/RestAPI.java
@@ -2,6 +2,6 @@ package com.github.williamgdev.example.submodules.restapi;
 
 public class RestAPI {
     public static void getValue(RestAPIListener<Integer> listener){
-        listener.onResult(5);
+        listener.onResult(555);
     }
 }
